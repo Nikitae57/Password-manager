faab6fa97818703101781f2ffa2695a05567beab
Evdokimov N.A <nikita-e57@yandex.ru>
2017-12-01 21:45:32 +0300

    Начало проекта. Добавлен первоначальный GUI

---
+++ src/Controllers/MainController.java
@@ -0,0 +1,29 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainController {
+
+    public void addNewNote(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../FXML/EditWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setTitle("Добавление записи");
+            stage.setScene(new Scene(root, 315, 170));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.setResizable(false);
+            stage.initOwner(( (Node) actionEvent.getSource()).getScene().getWindow());
+            stage.show();
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+    }
+}
---
+++ src/LogicClasses/Main.java
@@ -0,0 +1,25 @@
+package LogicClasses;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../FXML/MainWindow.fxml"));
+        primaryStage.setTitle("Менеджер паролей");
+        primaryStage.setMinWidth(420);
+        primaryStage.setMinHeight(260);
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
