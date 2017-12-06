c9d483b6d68b4330f39b44cd4716c32c5d3012de
Nikitae57 <nikita-e57@yandex.ru>
2017-12-06 12:48:25 +0300

    Добавил тесты для методов getNoteByServiceName и getNoteObservableList

--- Tests/tests/CollectionsPasswordManagerTest.java
+++ Tests/tests/CollectionsPasswordManagerTest.java
@@ -59,4 +59,32 @@ public class CollectionsPasswordManagerTest {
         assertEquals(cpwm.size(), 0);
     }
 
+    @Test
+    public void getNoteByServiceName() throws Exception {
+        CollectionsPasswordManager cpwm = new CollectionsPasswordManager();
+
+        Note note = new Note("google", "login", "password");
+        Note note2 = new Note("yandex", "login2", "password2");
+
+        cpwm.add(note);
+        cpwm.add(note2);
+
+        assertEquals(note, cpwm.getNoteByServiceName("google"));
+        assertNotEquals(note, cpwm.getNoteByServiceName("yandex"));
+    }
+
+    @Test
+    public void getNoteObservableList() throws Exception {
+        CollectionsPasswordManager cpwm = new CollectionsPasswordManager();
+
+        Note note = new Note("google", "login", "password");
+        Note note2 = new Note("yandex", "login2", "password2");
+
+        cpwm.add(note);
+        cpwm.add(note2);
+
+        assertEquals(note, cpwm.getNoteObservableList().get(0));
+        assertNotEquals(note2, cpwm.getNoteObservableList().get(0));
+    }
+
 }
